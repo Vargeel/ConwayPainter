1588a4c330583bebc41deccdbd37aa98c9bd79c7
Vargeel <antoine.larreche@gmail.com>
2022-01-31 09:20:05 +0000

    Fixes again

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:Vargeel/Vargeel.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Vargeel/ConwayPainter.git main:gh-pages
+git push -f https://github.com/Vargeel/ConwayPainter.git main:gh-pages
 
 cd -
